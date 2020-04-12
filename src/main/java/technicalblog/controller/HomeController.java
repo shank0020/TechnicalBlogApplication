2184a11bd50e040bc023ca6cdedaae1b5aa7aaca
shank0020 <shashank.cse20@gmail.com>
2020-04-12 06:56:38 +0530

    implemented jdbc functionality through branch2

--- src/main/java/technicalblog/controller/HomeController.java
+++ src/main/java/technicalblog/controller/HomeController.java
@@ -6,6 +6,7 @@ import technicalblog.model.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import java.util.*;
 
 @Controller
